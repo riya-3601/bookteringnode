c2036f60ceaa433f179b47b4c8d79b34b82ac990
riya-3601 <palkhiwala.riya@yahoo.com>
2021-02-08 18:36:03 +0530

    deleted practice files of model and routes

--- models/booksForSaleModel.js
+++
@@ -1,7 +0,0 @@
-var db=require('../dbconnection');
-var bfs={
-    getAllBooks:function(callback){
-        return db.query('select * from booksforsale_tbl',callback);
-    }
-}
-module.exports=bfs;
--- models/studentModel.js
+++
@@ -1,7 +0,0 @@
-var db=require('../dbconnection');
-var student={
-    getAllStudent:function(callback){
-        return db.query('select * from student_tbl',callback);
-    }
-};
-module.exports=student;
