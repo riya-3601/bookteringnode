6c1e57323c49870ab5bc1527bb3474477f22028c
sanjanashah9 <shahsanjana139@gmail.com>
2021-02-10 09:54:43 +0530

    order foreign key

--- models1/orderModel.js
+++ models1/orderModel.js
@@ -1,7 +1,7 @@
 var db=require('../dbconnection1');
 var orders={
     getAllOrders:function(callback){
-        return db.query('select * from order_tbl',callback);
+        return db.query('select o.*,c.* from order_tbl o,customer_tbl c where c.customer_id=o.fk_customer_id',callback);
     },
     addOrder:function(data,callback){
         return db.query('insert into order_tbl values (?,?,?,?,?,?)',[null,data.order_date,data.order_status,data.order_paymenttype,data.order_totalamount,data.fk_customer_id],callback);
@@ -10,10 +10,14 @@ var orders={
         return db.query('delete from order_tbl where order_id=?',[id],callback);
     },
     editOrder:function(data,callback){
-        return db.query('update order_tbl set order_date=?,order_status=?,order_paymenttype=?,order_totalamount=?,fk_customer_id where order_id=?',[data.order_date,data.order_status,data.order_paymenttype,data.order_totalamount,data.fk_customer_id,data.order_id],callback);
+        return db.query('update order_tbl set order_date=?,order_status=?,order_paymenttype=?,order_totalamount=?,fk_customer_id=? where order_id=?',[data.order_date,data.order_status,data.order_paymenttype,data.order_totalamount,data.fk_customer_id,data.order_id],callback);
     },
     getOrderById:function(id,callback){
         return db.query('select * from order_tbl where order_id=?',[id],callback);
+    },
+    getOrderbyCustomerId:function(data,callback){
+        return db.query('select o.*,c.* from order_tbl o,customer_tbl c where c.customer_id=o.fk_customer_id and o.order_id=?',[data.order_id],callback);
+
     }
 };
-module.exports=orders;
+module.exports=orders;  
