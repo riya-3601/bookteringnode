657440a3aa4063a4288161db3b60138d0fbf05ee
riya-3601 <palkhiwala.riya@yahoo.com>
2021-02-18 23:35:46 +0530

    getEmployeeDetails added

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ var orderdetailsRouter=require('./routes/orderdetailsRoutes');
 var orderdetailsbyorderidRouter=require('./routes/orderdetailsbyorderidRoute');
 var loginRouter=require('./routes/loginRoutes');
 var bookreviewbybookbarteridRouter=require('./routes/bookreviewbybookbarteridRoute');
+var getEmployeeDetailsRouter=require('./routes/getEmployeeDetailsRoutes');
 var app = express();
 
 // view engine setup
@@ -60,6 +61,7 @@ app.use('/employee',employeeRouter);
 app.use('/orderdetails',orderdetailsRouter);
 app.use('/orderdetailsbyorderid',orderdetailsbyorderidRouter);
 app.use('/bookreviewbybookbarterid',bookreviewbybookbarteridRouter);
+app.use('/getemployeeDetails',getEmployeeDetailsRouter);
 app.use('/login',loginRouter);
 
 // catch 404 and forward to error handler
