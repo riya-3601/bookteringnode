7bbd11663c08e895f20f4eca31efd82cf1c0d659
riya-3601 <palkhiwala.riya@gmail.com>
2021-03-17 12:16:38 +0530

    commit

--- models1/loginModel.js
+++ models1/loginModel.js
@@ -1,10 +1,7 @@
 var db1=require('../dbconnection1');
 var login={
     addadmin:function(data,callback){
-        return db1.query('select * from customer_tbl where customer_emailid=? and customer_password=? and customer_type=?',[data.login_username,data.login_password,0],callback);  
+        return db1.query('select * from customer_tbl where customer_emailid=? and customer_password=?',[data.login_username,data.login_password],callback);  
       },
-      checkCustomer:function(data,callback){
-        return db1.query('select * from customer_tbl where customer_emailid=? and customer_password=? and customer_type=?',[data.login_username,data.login_password,1],callback);
-      }
 };
 module.exports=login;  
